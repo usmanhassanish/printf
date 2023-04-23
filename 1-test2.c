3886555f6a7875f5f81ab516d31b8841be0b3416
ussyneh <usmanhassanish@gmail.com>
2023-04-23 08:13:43 -0700

    changes

--- 1-test2.c
+++ 1-test2.c
@@ -1,5 +1,5 @@
 #include <stdarg.h>
-
+#include "main.h"
 /**
  * README
  * i created the variadic function
@@ -23,24 +23,14 @@ int _printf(const char *format, ...)
 		{
 			case 's':
 				str = va_arg(ap, char *);
-				/* here i tried to use va_arg 
-				 * (since it is a variadic function)
-				 * to store the argument in str
-				 */
 				test(str);
-				/* here i called the test function to get the string
-				 * alvernatively i thought we could use the _putchar
-				 * to print the char in the string and 
-				 * store it in str and num counts the char's in str
-				 */
 				while (*str < '\0')
 					num += num;
 					num++;
 					*str++;
-				/* here i went through the sting to get num */
 				break;
 			case 'c':
-				str = "%c";
+				str = va_arg(ap, char *);
 				test(str);
 				while (*str < '\0')
 					num += num;
