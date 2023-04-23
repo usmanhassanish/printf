8d68a9b1b3335285d31cd985a360dc7e5aa07445
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-23 04:44:30 -0700

    test q0

--- 1-test2.c
+++ 1-test2.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "main.h"
 
 /**
  * README
@@ -12,8 +13,8 @@
 
 int _printf(const char *format, ...)
 {
-	int i, num = 0  /*num will count the charactors in the string*/;
-	char *str;  /*pointer to the string*/
+	int i, num = 0;
+	const char *str;
 	va_list ap;
 
 	va_start(ap, format);
@@ -21,44 +22,28 @@ int _printf(const char *format, ...)
 	{
 		switch (format[i])
 		{
-			case 's':
-				str = va_arg(ap, char *);
-				/* here i tried to use va_arg 
-				 * (since it is a variadic function)
-				 * to store the argument in str
-				 */
-				test(str);
-				/* here i called the test function to get the string
-				 * alvernatively i thought we could use the _putchar
-				 * to print the char in the string and 
-				 * store it in str and num counts the char's in str
-				 */
-				while (*str < '\0')
-					num += num;
-					num++;
-					*str++;
-				/* here i went through the sting to get num */
-				break;
 			case 'c':
-				str = "%c";
-				test(str);
-				while (*str < '\0')
-					num += num;
-					num++;
-					*str++;
+			{
+				num += test(va_arg(ap, int));
 				break;
+			}
+			case 's':
+			{
+				str = va_arg(ap, char *);
+				while(*str != '\0')
+				{
+					num += test(*str);
+					str++;
+				}
+				break;
+			}
 			case '%':
-				str = "%%";
-				test(str);
-				while (*str < '\0')
-					num += num;
-					num++;
-					*str++;
+			{
+				num += test('%');
 				break;
+			}
 			default:
-				return (0);
 				break;
-				
 		}
 	}
 	va_end(ap);
