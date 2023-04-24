90d87105cdd93b8e35529ae6e9958b6eb305bf53
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-23 23:39:47 -0700

    update

---
+++ 0-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+	int len;
+
+       	len = _printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	return (len);
+}
