032f47d42ba15e4f2d87132baa8db47ab1f44dd9
NqobileMdlalose <mdlalose150@gmail.com>
2023-04-25 00:31:53 -0700

    changes

--- _printf.c
+++ _printf.c
@@ -10,48 +10,50 @@
  * Return: we needed to return the number of charactors 
  * not the char themselves
  */
+int handle(char c, va_list ap)
+{
+	int num = 0, n;
+	char c;
+	char *s;
 
+	switch (c)
+	{
+		case 'c':
+				c =  va_arg(ap, int);
+				_putchar(c);
+				num++;
+				break;
+		case 's':
+				str = va_arg(ap, char *);
+				while (*str != '\0')
+				{
+					num += _putchar(str);
+					str++;
+				}
+		case 'i':
+		case 'd':
+			{
+				i = va_arg(ap, int);
+				num += 
+			}
+	}
+}
 int _printf(const char *format, ...)
 {
-	int i, num = 0;
-	const char *str;
 	va_list ap;
+	int num = 0;
 
 	va_start(ap, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format != '\0')
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			switch (format[i])
-			{
-				case 'c':
-				{
-					num += _putchar(va_arg(ap, int));
-					break;
-				}
-				case 's':
-				{
-					str = va_arg(ap, char *);
-					while(*str != '\0')
-					{
-						num += _putchar(*str);
-						str++;
-					}
-					break;
-				}
-				case '%':
-				{
-					num += _putchar('%');
-					break;
-				}
-				default:
-					break;
-			}
+			format++;
+			num += handle(*format, ap);
 		}
 		else
 		{
-			num += _putchar(*format);
+			_putchar(*format);
 		}
 		format++;
 	}
