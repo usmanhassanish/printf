7e3f7564e4487e174006b5050630db08027269ab
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2023-04-29 03:07:25 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 int _putuint(unsigned int num);
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _putoct(int n);
 char *to_binary(unsigned int num);
 int _putint(int n);
 unsigned int to_unsigned(int num);
