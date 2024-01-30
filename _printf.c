5c96939fbd59868ed2715450a6af72c2b5c8cdba
Usman I Hassan <83950818+usmanhassanish@users.noreply.github.com>
2024-01-30 11:43:05 +0100

    fixing task 1

--- _printf.c
+++ _printf.c
@@ -1,5 +1,49 @@
 #include "main.h"
 #include <stdarg.h>
+
+/**
+ * _itoa - converts an integer to a string
+ * @value: the integer value to convert
+ * @buffer: buffer to store the result
+ * @base: base for conversion (e.g., 10 for decimal)
+ * Return: pointer to the resulting string
+ */
+char *_itoa(int value, char *buffer, int base)
+{
+	char *ptr = buffer;
+	int temp_value;
+	int num_digits = 0;
+
+	if (value == 0)
+	{
+		*ptr++ = '0';
+		*ptr = '\0';
+		return buffer;
+	}
+
+	if (value < 0)
+	{
+		*ptr++ = '-';
+		value = -value;
+	}
+
+	temp_value = value;
+	while (temp_value)
+	{
+		temp_value /= base;
+		num_digits++;
+	}
+	ptr += num_digits;
+	*ptr = '\0';
+
+	while (value)
+	{
+		int digit = value % base;
+		*--ptr = digit + '0';
+		value /= base;
+	}
+	return buffer;
+}
 /**
  * _printf - custom printf
  * @format: format specifier
@@ -41,6 +85,21 @@ int _printf(const char *format, ...)
 				}
 				i++;
 			}
+			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+            		{
+				int j = 0;
+				
+        			int value = va_arg(ap, int);
+                		char buffer[12];
+                		_itoa(value, buffer, 10);
+                		while (buffer[j] != '\0')
+                		{
+                    			_putchar(buffer[j]);
+                    			j++;
+                    		charsP++;
+                		}
+                		i++;
+            		}
 			else if (format[i + 1] == '%')
 			{
 				_putchar('%');
